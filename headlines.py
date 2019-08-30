5e451b920ca78fa1c292e9ec63c4a7bb51876824
AlexanderTyan <at1079@georgetown.edu>
2019-08-30 09:19:52 -0500

    working through the tutorial part 1

---
+++ headlines.py
@@ -0,0 +1,12 @@
+# headlines.py
+
+
+def headline(text: str, centered: bool = False):
+    if not centered:
+        return f"{text.title()}\n{'-' * len(text)}"
+    else:
+        return f" {text.title()} ".center(50, "o")
+
+
+print(headline("python type checking"))
+print(headline("use mypy", centered=True))
